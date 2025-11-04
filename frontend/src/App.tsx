3be1996de3fca435ad11de1b99587ef49fe3c805
anthony-career <career.anthonyamponsah@gmail.com>
2025-11-04 10:31:26 +0000

    added tailwind setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className='h-screen w-screen bg-slate-300'>
+      <h1 className='text-4xl font-bold text-center text-slate-900'>
+        Welcome to the lema assignment
+      </h1>
+    </main>
+  );
+}
+
+export default App;
